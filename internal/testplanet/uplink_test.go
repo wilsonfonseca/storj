0ba01e234ac1b1509b2aaeae67cc4102a383685b
Egon Elbre <egonelbre@gmail.com>
2019-07-01 11:43:24 +0300

    revert params

--- internal/testplanet/uplink_test.go
+++ internal/testplanet/uplink_test.go
@@ -28,8 +28,8 @@ import (
 )
 
 func TestUplinksParallel(t *testing.T) {
-	const uplinkCount = 5
-	const parallelCount = 5
+	const uplinkCount = 3
+	const parallelCount = 2
 
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 6, UplinkCount: uplinkCount,
